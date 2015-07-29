7999d48c47c1b5501275392e0400943f627c2e27
Mark Danese <mark@outins.com>
2015-07-29 00:40:49 -0700

    added data and a working example

--- R/create_datasets.R
+++ R/create_datasets.R
@@ -44,16 +44,21 @@ load_nhanes <- function(f = "", yr, data_dir = "./data", lab = FALSE){
 
 #' Load and merge NHANES files
 #'
-#' Takes vector of NHANES file names, loads them, and merges them all by SEQN.  Automatically loads demo, DO NOT include "demo" in the character vector.
+#' Takes vector of NHANES file names, loads them, and merges them all by SEQN.  The function automatically loads the demo file, so you do not have to include "demo" in the character vector.  If you do include demo, or if you accidentally include the same filename twice, the function will only return a single copy of each file.
 #'
-#' @param vec_of_files A character vector of NHANES files (e.g., c("mcq", "biopro")) that identifies the stem of the desired file(s).  The demo file is ALWAYS included because it has the survey weights.  This vector should not include the final letter (e.g., _c) that indicates the wave (see yr).
+#' @param vec_of_files A character vector of NHANES files (e.g., c("mcq", "biopro")) that identifies the stem of the desired file(s).  The demo file is ALWAYS included because it has the survey weights.  This vector should NOT include the final letter (e.g., _c) that indicates the wave (see yr).
 #' @param yr The year for which the file should be extracted.
 #' @param data_dir The directory in which all of your NHANES subdirectories (one for each year) reside.  Default is the project "data" subdirectory (i.e., "./data") but if your data are elsewhere, you must specify the directory.  Keep in mind that this will be a directory that contains all of your NHANES subdirectories.  It is NOT the subdirectory for a specific year.  Do not use a slash (/) at the end.
 #' @return Returns a dataframe (which is also a data.table) with one column for each variable in each file requested.
-#' @examples \dontrun{
+#' @examples
+#' nhanes_directory <- paste0(system.file(package = "nhanes.tools"), "/extdata") # location of data in package extdata directory
+#' files_to_load <- c("mcq") # demo doesn't need to be included and will always be returned
+#' full <- load_merge(files_to_load, 2009, nhanes_directory)
+#' dim(full)
+#' \dontrun{
 #' # Example:  load many files listed in character vector
 #' listing <- c("mcq", "dex", "hcq", "hiq", "vix", "uc") # demo is assumed
-#' full <- load_merge(listing, 2003)
+#' full <- load_merge(listing, 2003, "./data")
 #' }
 #' @export
 load_merge <- function(vec_of_files, yr, data_dir = "./data") {
